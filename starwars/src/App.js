e3bcefdae35fd2b0db93602f0f25dc277d270d5a
JCRN <clemons.jason@gmail.com>
2019-07-19 10:30:32 -0700

    WIP: added character card headers component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import "./App.css";
+import Card from "./components/CharacterCard/Card";
 
 const App = () => {
-  const [people, setPeople] = useState();
+  const [people, setPeople] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,14 +14,20 @@ const App = () => {
   // sync up with, if any.
   useEffect(() => {
     axios
-      .get(`https://swapi.co/api/people/10/`)
-      .then(result => setPeople(result.data))
+      .get(`https://swapi.co/api/people/`)
+      .then(result => setPeople(result.data.results))
       .catch(error => console.log("ERROR: ", error));
   }, []);
-  console.log(people);
+
+  console.log("People line 22: ", people);
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <div className="card-container">
+        {people.map((person, index) => (
+          <Card person={person} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
--- starwars/src/components/CharacterCard/Card.js
+++ starwars/src/components/CharacterCard/Card.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Name from "./Name";
 
 export default function Card({ person }) {
-  return <div />;
+  return (
+    <div>
+      <Name person={person} />
+    </div>
+  );
 }
