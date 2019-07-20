201b17db70e7f18288b4bd13453467f93538a774
JCRN <clemons.jason@gmail.com>
2019-07-19 17:01:58 -0700

    mvp finished

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Table } from "semantic-ui-react";
+
+import "./App.css";
 import "./stylesheets/main.scss";
 import Card from "./components/Card";
 
 const App = () => {
   const [people, setPeople] = useState([]);
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
   useEffect(() => {
     axios
       .get(`https://swapi.co/api/people/`)
@@ -22,14 +20,32 @@ const App = () => {
       .catch(error => console.log("ERROR: ", error));
   }, []);
 
-  console.log(people);
   return (
-    <div className="App">
-      <div className="card-container">
-        {people.map((person, index) => (
-          <Card person={person} key={index} />
-        ))}
-      </div>
+    <div className="app-container">
+      <Table celled>
+        <Table.Header>
+          <Table.Row className="header-row">
+            <Table.HeaderCell className="header-name">Name</Table.HeaderCell>
+            <Table.HeaderCell className="header-gender">
+              Gender
+            </Table.HeaderCell>
+            <Table.HeaderCell className="header-year">
+              Birth Year
+            </Table.HeaderCell>
+            <Table.HeaderCell className="header-height">
+              Height
+            </Table.HeaderCell>
+            <Table.HeaderCell className="header-eyes">
+              Eye Color
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {people.map((person, index) => (
+            <Card person={person} key={index} />
+          ))}
+        </Table.Body>
+      </Table>
     </div>
   );
 };
